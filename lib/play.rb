c717f295864d8fbcd1e8a1e1e4b37c4468c21b42
Matt Wolford <matthew.r.wolford@gmail.com>
2021-01-03 21:35:46 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -35,4 +35,13 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+  counter = 0
+  loop do
+    turn(board)
+    counter = counter + 1
+    if counter == 9
+      break
+    end
+  end
+end
